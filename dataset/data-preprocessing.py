194c15933ec14b51d924625fd0bd928cd7d0c0d1
Farooq Ansari <farooq.ansari21297@gmail.com>
2020-05-10 14:17:13 +0530

    feature selection

--- dataset/data-preprocessing.py
+++ dataset/data-preprocessing.py
@@ -1,13 +1,15 @@
 import pandas as pd
+import numpy as np
 from sklearn.model_selection import train_test_split
 from sklearn import preprocessing
+from sklearn.impute import SimpleImputer
 
 # loading the dataset 
 data = pd.read_csv('Indian Liver Patient Dataset (ILPD).csv')
 
 # adding feature names
 data.columns = ['age', 'gender', 'total_bilirubn', 'direct_bilirubin', 'alk_phos',\
-				'sgpt', 'sgot', 'total_protiens', 'albumin', 'ratio_al_gl', 'target']
+				'sgpt', 'sgot', 'total_protein', 'albumin', 'ratio_al_gl', 'target']
 
 
 # standardizing data format - Gender
@@ -15,6 +17,15 @@ data['gender'] = data['gender'].replace({'Male': 0, 'Female': 1})
 
 # standardizing data format - Target
 data['target'] = data['target'].replace({1: 0, 2: 1})
+
+# this shows if there are any invalid 'NaN' values in the data
+# print(np.any(np.isnan(data)))
+
+# removing invalid values
+data = data.replace(" ",np.NaN)
+imp = SimpleImputer(missing_values=np.NaN, strategy='mean')
+imp.fit(data)
+data = pd.DataFrame(imp.transform(data))
 
 # splitting it to train and test data
 X, y = data.iloc[:, :-1],  data.iloc[:, -1]
@@ -26,8 +37,8 @@ X_train = pd.DataFrame(scaler.fit_transform(X_train))
 X_test = pd.DataFrame(scaler.transform(X_test))
 
 # adding columns
-X_train.columns = ['age', 'gender', 'total_bilirubn', 'direct_bilirubin', 'alk_phos','sgpt', 'sgot', 'total_protiens', 'albumin', 'ratio_al_gl']
-X_test.columns = ['age', 'gender', 'total_bilirubn', 'direct_bilirubin', 'alk_phos','sgpt', 'sgot', 'total_protiens', 'albumin', 'ratio_al_gl']
+X_train.columns = ['age', 'gender', 'total_bilirubn', 'direct_bilirubin', 'alk_phos','sgpt', 'sgot', 'total_protein', 'albumin', 'ratio_al_gl']
+X_test.columns = ['age', 'gender', 'total_bilirubn', 'direct_bilirubin', 'alk_phos','sgpt', 'sgot', 'total_protein', 'albumin', 'ratio_al_gl']
 
 # saving 
 data.to_csv('dataset.csv', index = False)
@@ -37,3 +48,7 @@ X_test.to_csv('X_test.csv', index = False)
 
 y_train.to_csv('y_train.csv', index = False)
 y_test.to_csv('y_test.csv', index = False)
+
+
+
+# print(np.any(np.isnan(data))) # to check again
---
+++ dataset/feature-selection.py
@@ -0,0 +1,59 @@
+import pandas as pd
+import numpy as np
+
+from sklearn.metrics import accuracy_score
+from sklearn.model_selection import train_test_split
+from itertools import combinations
+
+from sklearn import preprocessing
+from sklearn.linear_model import LogisticRegression
+
+max_acc = 0
+headers = ['age', 'gender', 'total_bilirubn', 'direct_bilirubin', 'alk_phos',\
+				'sgpt', 'sgot', 'total_protein', 'albumin', 'ratio_al_gl']
+
+for i in range(1,11): # no. of columns at a time
+
+	for columns in combinations(headers,i): # all combinations of i columns
+
+		columns = list(columns)
+
+		X_train = pd.read_csv('X_train.csv')
+		X_test = pd.read_csv('X_test.csv')
+
+		y_train = pd.read_csv('y_train.csv')
+		y_test = pd.read_csv('y_test.csv')
+
+		X_train = X_train[columns]
+		X_test = X_test[columns]
+
+		model = LogisticRegression(random_state = 2) 
+		model.fit(X_train, y_train.values.ravel())
+
+		# .values will give the values in an array. (shape: (n,1)
+		# .ravel() will convert that array shape to (n, )
+
+
+		y_pred = model.predict(X_test)
+
+		# print(X_train.shape, X_test.shape)
+		# accuracy = round(float((model.score(X_test, y_test)*100)),2)
+
+		accuracy = round(accuracy_score(y_test.values.ravel(), y_pred)*100, 2)
+
+		if accuracy > max_acc:
+
+			max_acc = accuracy
+			best_columns = columns
+			print("max till now", max_acc)
+
+
+
+print("Accuracy: ", max_acc, ' with ', best_columns)
+
+# saving the selected dataset
+
+X_train_best = X_train[best_columns].to_csv('X_train_best.csv', index = False) 
+X_test_best = X_test[best_columns].to_csv('X_test_best.csv', index = False) 
+
+
