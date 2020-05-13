f1af6e09945290de6e98cbaa83ff0c4502b8bc72
Farooq Ansari <farooq.ansari21297@gmail.com>
2020-05-14 00:34:57 +0530

    fixed integration bugs

--- model/predict.py
+++ model/predict.py
@@ -5,10 +5,17 @@ import os
 
 from sklearn.metrics import accuracy_score
 from sklearn.linear_model import LogisticRegression
+from sklearn import preprocessing
 
 def predict(query, X_train, y_train):
 
 	# the features were learned by the Genetic Algorithm
+	query = np.array(query).reshape(1, -1)
+
+	scaler = preprocessing.StandardScaler()
+	X_train = pd.DataFrame(scaler.fit_transform(X_train))
+	query = scaler.transform(query)
+
 	model = LogisticRegression(C = 100000, tol = 0.1, penalty = 'l1', solver = 'liblinear', max_iter = 100000, random_state = 13361) 
 	model.fit(X_train, y_train.values.ravel())
 	y_pred = model.predict(query)
@@ -17,11 +24,10 @@ def predict(query, X_train, y_train):
 	safe, unsafe = model.predict_proba(np.array(X_test.iloc[0, :]).reshape(1, -1))[0]
 	return round(safe*100, 2)
 
-data_path = "D:\\CSE\\Projects\\bioinformatics\\dataset"
+data_path = "..\\dataset"
 X_train =  pd.read_csv(os.path.join(data_path, 'X_train_best.csv'))
 y_train =  pd.read_csv(os.path.join(data_path, 'y_train.csv'))
-X_test =  pd.read_csv(os.path.join(data_path, 'X_test_best.csv'))
-query = np.array(X_test.iloc[0, :]).reshape(1, -1)
+query = [62, 0, 5.5, 64, 100, 0.74]
 
 print(query)
 print(predict(query, X_train, y_train))
